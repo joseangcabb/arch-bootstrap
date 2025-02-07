c42e791722eda85e50c8fdd567b51175bb611604
Jose Angel <josepalacioscab@gmail.com>
2025-02-07 00:33:28 -0500

    Add parameters for disk partition

--- tasks/disk.sh
+++ tasks/disk.sh
@@ -13,9 +13,9 @@ sgdisk --zap-all /dev/sda
 wipefs --all /dev/sda
 
 # Create partitions
-sgdisk /dev/sda --new=1:0:+500M --typecode=1:ef00 --change-name=1:EF_System
-sgdisk /dev/sda --new=2:0:+30G --typecode=2:8300 --change-name=2:Root_Filesystem
-sgdisk /dev/sda --new=3:0:+8G --typecode=3:8200 --change-name=3:Swap_Space
+sgdisk /dev/sda --new=1:0:+$1 --typecode=1:ef00 --change-name=1:EF_System
+sgdisk /dev/sda --new=2:0:+$2 --typecode=2:8300 --change-name=2:Root_Filesystem
+sgdisk /dev/sda --new=3:0:+$3 --typecode=3:8200 --change-name=3:Swap_Space
 sgdisk /dev/sda --new=4:0: --typecode=4:8300 --change-name=4:Home_Directory
 
 # Synchronize kernel partition table
