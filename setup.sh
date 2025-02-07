ac486340f8eb6c6f65e5b14c892117ebd972d8c8
Jose Angel <josepalacioscab@gmail.com>
2025-02-07 00:42:51 -0500

    Run disk.sh with parameters

--- setup.sh
+++ setup.sh
@@ -7,13 +7,16 @@ echo "=========================================="
 echo "$script_name (Version: $script_version)"
 echo "=========================================="
 
-show_loading "Partitioning disk" &
-loading_pid=$!
-bash ./scripts/disk.sh
-if [[ $? -eq 0 ]]; then
-  stop_loading "$loading_pid" "Disk partitioning completed successfully!"
-else
-  stop_loading "$loading_pid" "Disk partitioning failed!"
-  exit 1
-fi
+# Disk partitions
+read -p "Size for EFI (default 500M): " efi_size
+efi_size=${efi_size:-500M}
 
+read -p "Size for Root (default 50G): " root_size
+root_size=${root_size:-50G}
+
+read -p "Size for Swap (default 8G): " swap_size
+swap_size=${swap_size:-8G}
+
+execute_with_loading \
+  "bash ./scripts/disk.sh '$efi_size' '$root_size' '$swap_size'" \
+  "Disk partition"
