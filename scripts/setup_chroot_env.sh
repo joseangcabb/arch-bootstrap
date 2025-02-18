b402316aa9bf08058254768a7defce63c90c1aa7
Jose Angel <josepalacioscab@gmail.com>
2025-02-17 23:59:06 -0500

    Fix chroot path

--- scripts/setup_chroot_env.sh
+++ scripts/setup_chroot_env.sh
@@ -3,8 +3,8 @@
 
 # Copy setup scripts into the chroot environment
 mkdir -p "/mnt${CHROOT_SCRIPTS_DIR}"
-cp -rv "${HOST_SCRIPTS_DIR}/chroot/." "/mnt${CHROOT_SETUP_DIR}"
-cp -v "/tmp/${USER_SETTINGS}" "/mnt${CHROOT_SETUP_DIR}"
+cp -rv "${HOST_SCRIPTS_DIR}/chroot/." "/mnt${CHROOT_SCRIPTS_DIR}"
+cp -v "/tmp/${USER_SETTINGS}" "/mnt${CHROOT_SCRIPTS_DIR}"
 
 # Install base system
 pacstrap /mnt base linux linux-firmware
