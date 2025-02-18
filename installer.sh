92b7af3ad9d34a7ff8339f0fb25577306919f635
Jose Angel <josepalacioscab@gmail.com>
2025-02-17 21:35:43 -0500

    Add services

--- installer.sh
+++ installer.sh
@@ -7,7 +7,12 @@ main() {
   bash "${HOST_SCRIPTS_DIR}/prepare_disk.sh"
   bash "${HOST_SCRIPTS_DIR}/setup_chroot_env.sh"
 
+  arch-chroot /mnt /bin/bash -c "bash ${CHROOT_SCRIPTS_DIR}/timezone.sh"
   arch-chroot /mnt /bin/bash -c "bash ${CHROOT_SCRIPTS_DIR}/locale.sh"
+  arch-chroot /mnt /bin/bash -c "bash ${CHROOT_SCRIPTS_DIR}/hostname.sh"
+  arch-chroot /mnt /bin/bash -c "bash ${CHROOT_SCRIPTS_DIR}/users.sh"
+  arch-chroot /mnt /bin/bash -c "bash ${CHROOT_SCRIPTS_DIR}/bootloader.sh"
+  arch-chroot /mnt /bin/bash -c "bash ${CHROOT_SCRIPTS_DIR}/services.sh"
 }
   
 main "@"
