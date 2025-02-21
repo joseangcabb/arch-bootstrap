2c0ef5fac32b56db34f9e90f6ec574164efff78d
Jose Angel <josepalacioscab@gmail.com>
2025-02-20 21:38:22 -0500

    Remove unnecessary functions

--- functions.sh
+++ functions.sh
@@ -1,52 +1,17 @@
 #!/bin/bash
 
-error_exit() {
-  echo "Error $1" >&2
-}
-
-validate_file() {
-  if [[ ! -f "$1" ]]; then
-    error_exit "File '$1' does not exist."
-  fi
-}
-
-validate_directory() {
-  if [[ ! -d "$1" ]]; then
-    error_exit "Directory '$1' does not exist."
-  fi
-}
-
-show_loading() {
-  local msg="$1"
-  local delay=0.1
-  local spinchars='|/-\'
+read_password() {
+  local prompt=$1 var=$2
   while true; do
-    for ((i=0; i<${#spinchars}; i++)); do
-      echo -ne "\r${msg} ${spinchars:$i:1}"
-      sleep "$delay"
-    done
+    read -rsp "${prompt}: " "$var"
+    echo
+    read -rsp "Confirm ${prompt}: " confirm
+    echo
+    if [[ "${!var}" == "$confirm" ]]; then
+      break
+    fi
+    echo "Passwords do not match. Try again."
   done
 }
 
-stop_loading() {
-  kill "$1" 2>/dev/null
-  echo -e "\r$2"
-}
 
-execute_with_loading() {
-  local command="$1"
-  local command_desc="$2"
-
-  show_loading "$command_desc" &
-  local loading_pid=$!
-
-  bash -c "$command"
-  local exit_code=$?
-
-  if [[ $exit_code -eq 0 ]]; then
-    stop_loading "$loading_pid" "[OK] $command_desc"
-  else
-    stop_loading "$loading_pid" "[ERROR] $command_desc"
-    return 1
-  fi
-}
