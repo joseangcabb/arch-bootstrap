4ccba4910908c043bdc5b460dfa44979206bbc03
Jose Angel <josepalacioscab@gmail.com>
2025-04-18 03:06:22 -0500

    Refactor config

--- config.sh
+++ config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export HOST_SCRIPTS_DIR="$(pwd)/scripts"
-export CHROOT_SCRIPTS_DIR="/root/setup_scripts"
+export SCRIPTS="$(pwd)/scripts"
+export CHROOT_FILES="$(pwd)/chroot"
 
-# Temporary settings used in the installation of arch linux
-export INSTALL_SETTINGS_FILE="tmp_settings.conf"
+# Temporary files
+export SETTINGS_FILE="tmp_settings.conf"
+export INSTALLATION_FILE="tmp_installation.sh"
