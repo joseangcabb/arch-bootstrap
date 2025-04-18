a9270d633afc33faec45e8b96d2fb391ba048b72
Jose Angel <josepalacioscab@gmail.com>
2025-04-18 13:11:26 -0500

    mend
    Fix script file name

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -5,35 +5,17 @@ source ./functions.sh || {
   exit 1
 }
 
-# Default partition sizes
-DEFAULT_EFI_SIZE="500M"
-DEFAULT_ROOT_SIZE="50G"
-DEFAULT_SWAP_SIZE="8G"
-
-# Default values for localization
-DEFAULT_TIMEZONE="America/Lima"
-DEFAULT_LANG="en_US.UTF-8"
-
 echo -e "${MAGENTA}SETTINGS${NC}"
 
 echo -e "${GREEN}>>> System settings...${NC}"
 read -rp "Hostname: " hostname 
+read -rp "Timezone (e.g. America/New_York): " timezone
+read -rp "Language (e.g. en_US.UTF-8): " lang
 
-read -rp "Timezone [${DEFAULT_TIMEZONE}]: " timezone
-timezone="${timezone:-${DEFAULT_TIMEZONE}}"
-
-read -rp "Language [${DEFAULT_LANG}]: " lang
-lang="${lang:-${DEFAULT_LANG}}"
-
-echo -e "${GREEN}>>> Partition sizes (500M, 50G, etc)...${NC}"
-read -rp "Size for EFI [${DEFAULT_EFI_SIZE}]: " efi_size
-efi_size="${efi_size:-${DEFAULT_EFI_SIZE}}"
-
-read -rp "Size for Root [${DEFAULT_ROOT_SIZE}]: " root_size
-root_size="${root_size:-${DEFAULT_ROOT_SIZE}}"
-
-read -rp "Size for Swap [${DEFAULT_SWAP_SIZE}]: " swap_size
-swap_size="${swap_size:-${DEFAULT_SWAP_SIZE}}"
+echo -e "${GREEN}>>> Partition sizes...${NC}"
+read -rp "Size for EFI (e.g. 500M): " efi_size
+read -rp "Size for Root (e.g. 50G): " root_size
+read -rp "Size for Swap(e.g. 8G): " swap_size
 
 echo -e "${GREEN}>>> Root settings...${NC}"
 read_password "Root Password: " root_password
