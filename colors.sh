5eca22ffb58aa66952d2b7862130873770304493
Jose Angel <josepalacioscab@gmail.com>
2025-04-18 02:49:00 -0500

    Add more colors

--- colors.sh
+++ colors.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+export BLACK="\033[0;30m"
 export RED="\033[0;31m"
 export GREEN="\033[0;32m"
+export YELLOW="\033[0;33m"
+export BLUE="\033[0;34m"
+export MAGENTA="\033[0;35m"
+export CYAN="\033[0;36m"
+export WHITE="\033[0;37m"
+
 export NC="\033[0m" # Reset color
